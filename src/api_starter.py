f537706d81cc3f0eef8d9509a02af87352f68bbd
Bryan Montecino <lizamamobj@vcu.edu>
2023-12-11 15:13:01 -0500

    updating API for rubric

--- src/api_starter.py
+++ src/api_starter.py
@@ -1,25 +1,55 @@
 from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+import datetime
 
 app = Flask(__name__)
 
-# Sample data
+
+app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'
+jwt = JWTManager(app)
+
+# Added JWT authentication with a login endpoint.
+# The @jwt_required() decorator secures the endpoints, requiring a valid JWT token for access.
+
+# Data structures
 users = {}
 budgets = {}
 actuals = {}
 transactions = {}
 
+# Utility function for filtering data
+def filter_data(data, filters):
+    return {key: value for key, value in data.items() if all(value.get(k) == v for k, v in filters.items())}
+
 # User Endpoints
+@app.route('/login', methods=['POST'])
+def login():
+    # Implement your authentication logic here
+    # This is a placeholder for demonstration
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if username != 'admin' or password != 'password':
+        return jsonify({"msg": "Bad username or password"}), 401
+    expires = datetime.timedelta(days=7)
+    access_token = create_access_token(identity=username, expires_delta=expires)
+    return jsonify(access_token=access_token), 200
+
 @app.route('/users', methods=['GET'])
+@jwt_required()
 def get_users():
-    return jsonify(users)
+    query_params = request.args
+    filtered_users = filter_data(users, query_params)
+    return jsonify(filtered_users)
 
 @app.route('/users', methods=['POST'])
+@jwt_required()
 def create_user():
     user = request.json
     users[user['userID']] = user
     return jsonify(user), 201
 
 @app.route('/users/<userID>', methods=['PUT'])
+@jwt_required()
 def update_user(userID):
     if userID not in users:
         return jsonify({'error': 'User not found'}), 404
@@ -27,24 +57,40 @@ def update_user(userID):
     return jsonify(users[userID])
 
 @app.route('/users/<userID>', methods=['DELETE'])
+@jwt_required()
 def delete_user(userID):
     if userID not in users:
         return jsonify({'error': 'User not found'}), 404
     del users[userID]
     return jsonify({'message': 'User deleted successfully'})
 
-# Budgets Endpoints
+# Repeat the structure for budgets, actuals, and transactions
+# For instance:
+
 @app.route('/budgets', methods=['GET'])
+@jwt_required()
 def get_budgets():
-    return jsonify(budgets)
+    query_params = request.args
+    filtered_budgets = filter_data(budgets, query_params)
+    return jsonify(filtered_budgets)
+
+# Budget Endpoints
+@app.route('/budgets', methods=['GET'])
+@jwt_required()
+def get_budgets():
+    query_params = request.args
+    filtered_budgets = filter_data(budgets, query_params)
+    return jsonify(filtered_budgets)
 
 @app.route('/budgets', methods=['POST'])
+@jwt_required()
 def create_budget():
     budget = request.json
     budgets[budget['budgetID']] = budget
     return jsonify(budget), 201
 
 @app.route('/budgets/<budgetID>', methods=['PUT'])
+@jwt_required()
 def update_budget(budgetID):
     if budgetID not in budgets:
         return jsonify({'error': 'Budget not found'}), 404
@@ -52,6 +98,7 @@ def update_budget(budgetID):
     return jsonify(budgets[budgetID])
 
 @app.route('/budgets/<budgetID>', methods=['DELETE'])
+@jwt_required()
 def delete_budget(budgetID):
     if budgetID not in budgets:
         return jsonify({'error': 'Budget not found'}), 404
@@ -60,16 +107,21 @@ def delete_budget(budgetID):
 
 # Actuals Endpoints
 @app.route('/actuals', methods=['GET'])
+@jwt_required()
 def get_actuals():
-    return jsonify(actuals)
+    query_params = request.args
+    filtered_actuals = filter_data(actuals, query_params)
+    return jsonify(filtered_actuals)
 
 @app.route('/actuals', methods=['POST'])
+@jwt_required()
 def create_actual():
     actual = request.json
     actuals[actual['actualID']] = actual
     return jsonify(actual), 201
 
 @app.route('/actuals/<actualID>', methods=['PUT'])
+@jwt_required()
 def update_actual(actualID):
     if actualID not in actuals:
         return jsonify({'error': 'Actual not found'}), 404
@@ -77,6 +129,7 @@ def update_actual(actualID):
     return jsonify(actuals[actualID])
 
 @app.route('/actuals/<actualID>', methods=['DELETE'])
+@jwt_required()
 def delete_actual(actualID):
     if actualID not in actuals:
         return jsonify({'error': 'Actual not found'}), 404
@@ -85,16 +138,21 @@ def delete_actual(actualID):
 
 # Transactions Endpoints
 @app.route('/transactions', methods=['GET'])
+@jwt_required()
 def get_transactions():
-    return jsonify(transactions)
+    query_params = request.args
+    filtered_transactions = filter_data(transactions, query_params)
+    return jsonify(filtered_transactions)
 
 @app.route('/transactions', methods=['POST'])
+@jwt_required()
 def create_transaction():
     transaction = request.json
     transactions[transaction['transID']] = transaction
     return jsonify(transaction), 201
 
 @app.route('/transactions/<transID>', methods=['PUT'])
+@jwt_required()
 def update_transaction(transID):
     if transID not in transactions:
         return jsonify({'error': 'Transaction not found'}), 404
@@ -102,12 +160,12 @@ def update_transaction(transID):
     return jsonify(transactions[transID])
 
 @app.route('/transactions/<transID>', methods=['DELETE'])
+@jwt_required()
 def delete_transaction(transID):
     if transID not in transactions:
         return jsonify({'error': 'Transaction not found'}), 404
     del transactions[transID]
     return jsonify({'message': 'Transaction deleted successfully'})
 
-# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
